868ebd58f5dd338e04b1c1687a6c206632da8bea
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-23 16:00:46 +0900

    [Feat] chapter 3-02 페이징 기능 추가하기

--- src/main/java/com/example/question/repository/QuestionRepository.java
+++ src/main/java/com/example/question/repository/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.example.question.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.question.model.Question;
@@ -11,4 +13,5 @@ public interface QuestionRepository extends JpaRepository<Question, Long>{
 	Question findBySubject(String subject);
 	Question findBySubjectAndContent(String subject, String content);
 	List<Question> findBySubjectLike(String subject);
+	Page<Question> findAll(Pageable pageable);
 }
