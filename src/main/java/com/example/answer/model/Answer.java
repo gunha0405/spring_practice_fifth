35526ec62951cda47555add1a6ad52a6921b6f21
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-23 15:28:52 +0900

    [Feat] chapter 2-12 답변 기능 만들기

--- src/main/java/com/example/answer/model/Answer.java
+++ src/main/java/com/example/answer/model/Answer.java
@@ -5,11 +5,17 @@ import java.time.LocalDateTime;
 import com.example.question.model.Question;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Answer {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
