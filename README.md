c2edbec33fb7795a57dce278c120bbb8f578b4e1
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-23 14:28:06 +0900

    git init

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring_practice_fifth
+# spring_practice_fifth
---
+++ src/test/java/com/example/SpringPracticeFifthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeFifthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
