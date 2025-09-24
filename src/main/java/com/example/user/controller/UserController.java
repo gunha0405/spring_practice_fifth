13ecebb3fef5c4e47a40ecccd64335b3144152f6
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-24 16:48:20 +0900

    [Feat] chapter 3-14-4 비밀번호 찾기와 변경 기능 추가

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.example.user.controller;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.Exception.DataNotFoundException;
+import com.example.user.model.CustomUserDetails;
 import com.example.user.model.dto.UserCreateForm;
 import com.example.user.service.UserService;
 
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -56,5 +63,44 @@ public class UserController {
     @GetMapping("/login")
     public String login() {
         return "login_form";
+    }
+    
+    @GetMapping("/forgot-password")
+    public String forgotPasswordForm() {
+    	return "forgot_password";
+    }
+    
+    @GetMapping("/change-password")
+    public String changePasswordForm() {
+    	return "change_password";
+    }
+    
+    @PostMapping("/forgot-password")
+    public String forgotPassword(@RequestParam("email") String email, Model model) {
+    	try {
+            String message = userService.resetPassword(email);
+            model.addAttribute("message", message);
+        } catch (DataNotFoundException e) {
+            model.addAttribute("error", e.getMessage());
+        } catch (MessagingException e) {
+			e.printStackTrace();
+		}
+        return "forgot_password";
+    }
+    
+    @PostMapping("/change-password")
+    @PreAuthorize("isAuthenticated()")
+    public String changePassword(@RequestParam("currentPassword") String currentPassword,
+                                @RequestParam("newPassword") String newPassword,
+                                @RequestParam("confirmPassword") String confirmPassword,
+                                @AuthenticationPrincipal CustomUserDetails userDetails, 
+                                Model model) {
+        try {
+            String message = userService.changePassword(userDetails.getUsername(), currentPassword, newPassword, confirmPassword);
+            model.addAttribute("message", message);
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", e.getMessage());
+        }
+        return "change_password";
     }
 }
