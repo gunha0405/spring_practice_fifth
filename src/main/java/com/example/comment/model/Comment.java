62ffd699852ef1eb07bec534a03966b371fa24c8
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-24 15:35:56 +0900

    [Feat] chapter 3-14-2 댓글 기능 추가하기

---
+++ src/main/java/com/example/comment/model/Comment.java
@@ -0,0 +1,61 @@
+package com.example.comment.model;
+
+import java.time.LocalDateTime;
+
+import com.example.answer.model.Answer;
+import com.example.question.model.QuestionA;
+import com.example.question.model.QuestionB;
+import com.example.question.model.QuestionType;
+import com.example.user.model.SiteUser;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	private SiteUser author;
+	
+	@Column(columnDefinition = "TEXT")
+	private String content;
+	
+	private LocalDateTime createDate;
+	
+	private LocalDateTime modifyDate;
+	
+	@ManyToOne
+	private QuestionA questionA;
+	
+	@ManyToOne
+	private QuestionB questionB;
+	
+	@ManyToOne
+	private Answer answer;
+	
+	public Long getQuestionId() {
+	    if (this.questionA != null) {
+	        return this.questionA.getId();
+	    } else if (this.questionB != null) {
+	        return this.questionB.getId();
+	    } else if (this.answer != null) {
+	        return this.answer.getQuestionId();
+	    }
+	    return null;
+	}
+
+	
+}
--- src/main/java/com/example/question/model/QuestionB.java
+++ src/main/java/com/example/question/model/QuestionB.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 
 import com.example.answer.model.Answer;
+import com.example.comment.model.Comment;
 import com.example.user.model.SiteUser;
 
 import jakarta.persistence.CascadeType;
@@ -45,6 +46,9 @@ public class QuestionB implements BaseQuestion{
 
     @ManyToMany
     private Set<SiteUser> voter;
+    
+    @OneToMany(mappedBy = "questionB")
+    private List<Comment> commentList;
     
     @Override
     public QuestionType getQuestionType() {
